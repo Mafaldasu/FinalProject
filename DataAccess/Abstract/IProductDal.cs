24057d2bd8a077bae5d0b7fb00646a089a96c405
Mafaldasu <65609413+Mafaldasu@users.noreply.github.com>
2021-02-09 23:03:34 +0300

    9 Şubat 2020

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,12 +1,17 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntitiyRepository<Product>
+    public interface IProductDal : IEntitiyRepository<Product>
     {
-        
+        List<ProductDetailDto> GetProductDetails();
+
     }
 }
+//Code Refactoring
